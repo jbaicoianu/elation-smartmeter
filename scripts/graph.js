97f3b88b9bf5af9644e95a75b443b0f5982388c9
James Baicoianu <james_github@baicoianu.com>
2012-11-19 00:19:13 -0800

    Plot gas alongside electricity

--- scripts/graph.js
+++ scripts/graph.js
@@ -4,69 +4,48 @@ elation.component.add('smartmeter.graph', function() {
     
     //var data = [1, 5, 4, 2, 6, 2, 9, 19, 32, 12, 4, 6];
     var data = [];
+    this.areas = {};
     var maxval = 0;
-    this.plot(this.args.intervals);
+    this.initgraph();
+    this.add("electric", this.args.intervals.electric, 'costperhour');
+    this.add("gas", this.args.intervals.gas, 'costperhour');
   }
-  
-  this.plot = function(data) {
-    // FIXME - somehow data is coming through as an object instead of an array, which d3 requires
-    //         so we convert it, but also use this opportunity to find the max value for this dataset
-    var arrdata = []; 
-    var maxval = 0;
 
-    var timerange = [0, 0];
-    for (var k in data) {
-      arrdata.push(data[k]);
-      data[k].time = new Date(data[k].start * 1000);
+  this.initgraph = function() {
+    var margin = {top: 30, right: 40, bottom: 100, left: 40},
+        margin2 = {top: 430, right: 40, bottom: 20, left: 40};
+    this.width = 960 - margin.left - margin.right;
 
-      if (data[k].start < timerange[0]) timerange[0] = data[k].start;
-      if (data[k].end > timerange[1]) timerange[1] = data[k].end;
-      
-      if (data[k].value > maxval) {
-        maxval = data[k].value;
-      }
-    }
-    
-    var margin = {top: 10, right: 10, bottom: 100, left: 40},
-        margin2 = {top: 430, right: 10, bottom: 20, left: 40},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom,
-        height2 = 500 - margin2.top - margin2.bottom;
+    this.height = 500 - margin.top - margin.bottom;
+    this.height2 = 500 - margin2.top - margin2.bottom;
 
-    var x = d3.time.scale().range([0, width]),
-        x2 = d3.time.scale().range([0, width]),
-        y = d3.scale.linear().range([height, 0]),
-        y2 = d3.scale.linear().range([height2, 0]);
+    this.initscales();
+    var x = d3.time.scale().range([0, this.width]),
+        x2 = d3.time.scale().range([0, this.width]),
+        y = d3.scale.linear().range([this.height, 0]),
+        y2 = d3.scale.linear().range([this.height2, 0]);
 
     var xAxis = d3.svg.axis().scale(x).orient("bottom"),
         xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
         yAxis = d3.svg.axis().scale(y).orient("left");
+        //yAxis2 = d3.svg.axis().scale(y).orient("right");
     
-    this.brush = d3.svg.brush()
-        .x(x2)
-        .on("brush", elation.bind(this, this.onbrush));
-
-    var area = d3.svg.area()
-        .interpolate("monotone")
-        .x(function(d) { var newx = x(d.time); return newx; })
-        .y0(height)
-        .y1(function(d) { var newy = y(d.value); return newy; });
-
-    var area2 = d3.svg.area()
-        .interpolate("monotone")
-        .x(function(d) { var newx = x2(d.time); return newx; })
-        .y0(height2)
-        .y1(function(d) { var newy = y2(d.value); return newy; });
-
     var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom);
+        .attr("width", this.width + margin.left + margin.right)
+        .attr("height", this.height + margin.top + margin.bottom);
 
     svg.append("defs").append("clipPath")
         .attr("id", "clip")
       .append("rect")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", this.width)
+        .attr("height", this.height);
+
+    this.xlabel = svg.append("text")
+      .attr("class", "x label")
+      .attr("text-anchor", "end")
+      .attr("x", this.width)
+      .attr("y", 20);
+
 
     this.focus = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -79,54 +58,150 @@ elation.component.add('smartmeter.graph', function() {
     this.x2 = x2;
     this.y2 = y2;
     this.xAxis = xAxis;
-    this.area = area;
+    this.xAxis2 = xAxis2;
+    this.yAxis = yAxis;
+    //this.yAxis2 = yAxis2;
 
-    var maxtime = d3.max(arrdata.map(function(d) { return d.time; }));
-    // default scale 7 days ago
-    x.domain([new Date(maxtime - 7 * 24 * 60 * 60 * 1000), maxtime]);
-    y.domain([0, d3.max(arrdata.map(function(d) { return d.value; }))]);
-    x2.domain(d3.extent(arrdata.map(function(d) { return d.time; })));
-    y2.domain(y.domain());
-
-    this.brush.extent(x.domain());
-
-    this.focus.append("path")
-      .data([arrdata])
-      .attr("clip-path", "url(#clip)")
-      .attr("d", area);
     this.focus.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+      .attr("transform", "translate(0," + this.height + ")")
+      .call(this.xAxis);
+    this.context.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + this.height2 + ")")
+      .call(this.xAxis2);
+
     this.focus.append("g")
       .attr("class", "y axis")
-      .call(yAxis);
+      .call(this.yAxis);
     this.focus.append("text")
-      .attr("class", "x label")
+      .attr("class", "y label")
       .attr("text-anchor", "end")
       .attr("x", 0)
       .attr("y", -2)
-      .text("Watts/h");
+      .text("$/h");
+/*
+    this.focus.append("g")
+      .attr("class", "y2 axis")
+      .attr("transform", "translate(" + this.width + ",0)")
+      .call(this.yAxis2);
+    this.focus.append("text")
+      .attr("class", "y2 label")
+      .attr("text-anchor", "end")
+      .attr("x", this.width)
+      .attr("y", -2)
+      .text("Therms/h");
+*/
+  }
+  this.initscales = function() {
+/*
+    var x = d3.time.scale().range([0, this.width]),
+        x2 = d3.time.scale().range([0, this.width]),
+        y = d3.scale.linear().range([this.height, 0]),
+        y2 = d3.scale.linear().range([this.height2, 0]);
+*/
+  }
+  
+  this.add = function(name, data, field) {
+    // FIXME - somehow data is coming through as an object instead of an array, which d3 requires
+    //         so we convert it, but also use this opportunity to find the max value for this dataset
+    var arrdata = []; 
+    var maxval = 0;
+    if (typeof field == 'undefined') field = 'cost';
+
+    var timerange = [0, 0];
+    for (var k in data) {
+      arrdata.push(data[k]);
+      data[k].time = new Date(data[k].start * 1000);
+
+      if (data[k].start < timerange[0]) timerange[0] = data[k].start;
+      if (data[k].end > timerange[1]) timerange[1] = data[k].end;
+      
+      if (data[k][field] > maxval) {
+        maxval = data[k][field];
+      }
+    }
+    var x = this.x, y = this.y, x2 = this.x2, y2 = this.y2, y3 = this.yy;
+    
+    var maxtime = d3.max(arrdata.map(function(d) { return d.time; }));
+    var mintime = d3.min(arrdata.map(function(d) { return d.time; }));
+    // default scale 7 days ago
+    this.setrange([new Date(maxtime - 7 * 24 * 60 * 60 * 1000), maxtime]);
+    x2.domain([mintime, maxtime]);
+
+    var maxval = d3.max(arrdata.map(function(d) { return d[field]; }));
+    var minval = d3.min(arrdata.map(function(d) { return d[field]; }));
+    var ydomain = y.domain();
+console.log(ydomain, [minval, maxval]);
+    y.domain([Math.min(ydomain[0], minval), Math.max((ydomain[1] == 1 ? 0 : ydomain[1]), maxval)]);
+    y2.domain(y.domain());
+
+    var area = d3.svg.area()
+        .interpolate("monotone")
+        .x(function(d) { var newx = x(d.time); return newx; })
+        .y0(this.height)
+        .y1(function(d) { var newy = y(d[field]); return newy; });
+
+    var area2 = d3.svg.area()
+        .interpolate("monotone")
+        .x(function(d) { var newx = x2(d.time); return newx; })
+        .y0(this.height2)
+        .y1(function(d) { var newy = y2(d[field]); return newy; });
+
+    this.areas[name] = area; 
+    this.areas[name + "_summary"] = area2; 
+
+    this.focus.append("path")
+      .attr("class", name)
+      .data([arrdata])
+      .attr("clip-path", "url(#clip)")
+      //.attr("d", area);
     this.context.append("path")
+      .attr("class", name)
       .data([arrdata])
       .attr("d", area2);
-    this.context.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height2 + ")")
-      .call(xAxis2);
+
+
+    this.focus.select(".x.axis").call(this.xAxis);
+    this.focus.select(".y.axis").call(this.yAxis);
+    //this.focus.select(".y2.axis").call(this.yAxis2);
+    this.context.select(".x.axis").call(this.xAxis2);
+
+    if (!this.brush) {
+      this.brush = d3.svg.brush()
+          .x(x2)
+          .on("brush", elation.bind(this, this.onbrush));
+      this.brush.extent(x.domain());
+    } else {
+      this.context.select(".x.brush").remove();
+      //this.brush.parentNode.removeChild(this.brush);
+    }
+
     this.context.append("g")
         .attr("class", "x brush")
         .call(this.brush)
       .selectAll("rect")
         .attr("y", -6)
-        .attr("height", height2 + 7);
+        .attr("height", this.height2 + 7);
+
+    this.setrange();
+  }
+  this.setrange = function(range) {
+    if (range) {
+      this.x.domain(range);
+    } else {
+      range = this.x.domain();
+    }
+    for (var k in this.areas) {
+      this.focus.select("path." + k).attr("d", this.areas[k]);
+    }
+    this.focus.select(".x.axis").call(this.xAxis);
+    this.xlabel.text(elation.utils.dateformat("D M j H:m", range[0]) + " - " + elation.utils.dateformat("D M j H:m", range[1]));
   }
   this.onbrush = function() {
     if (!this.brushtimer) {
       this.brushtimer = setTimeout(elation.bind(this, function() {
-        this.x.domain(this.brush.empty() ? this.x2.domain() : this.brush.extent());
-        this.focus.select("path").attr("d", this.area);
-        this.focus.select(".x.axis").call(this.xAxis);
+        this.setrange(this.brush.empty() ? this.x2.domain() : this.brush.extent());
         this.brushtimer = false;
       }), 1000/60);
     }
--- smartmeter.php
+++ smartmeter.php
@@ -7,21 +7,70 @@ class Component_smartmeter extends Component {
 
   public function controller_smartmeter($args) {
     $vars = array();
-    $fuh = simplexml_load_file("components/smartmeter/data/electric.xml");
-    $vars["intervals"] = array();
-    foreach ($fuh->entry as $k=>$entry) {
+    $vars["intervals"] = array(
+      "electric" => $this->getintervals("components/smartmeter/data/electric.xml"),
+      "gas" => $this->getintervals("components/smartmeter/data/gas.xml"),
+    );
+
+    return $this->GetComponentResponse("./smartmeter.tpl", $vars);
+  }
+  public function controller_weather($args) {
+    if (!empty($args["date"]) && !empty($args["location"])) {
+      $weather = $this->getweather($args["date"], $args["location"]);
+      $vars["intervals"] = array();
+      if (!empty($weather->history->observations)) {
+        foreach ($weather->history->observations as $obs) {
+          $obj = array(
+            "start" => mktime($obs->utcdate->hour, $obs->utcdate->min, 0, $obs->utcdate->mon, $obs->utcdate->mday, $obs->utcdate->year),
+            "value" => (float)$obs->tempi
+          );
+          array_push($vars["intervals"], $obj);
+        }
+      }
+    }
+    return $this->GetComponentResponse("./weather.tpl", $vars);
+  }
+
+
+  public function getintervals($fname) {
+    $xmldata = simplexml_load_file($fname);
+    $intervals = array();
+    foreach ($xmldata->entry as $k=>$entry) {
       if (!empty($entry->content->IntervalBlock)) {
         foreach ($entry->content->IntervalBlock->IntervalReading as $interval) {
           $obj = array(
             "start" => (int) $interval->timePeriod->start,
             "duration" => (int) $interval->timePeriod->duration,
-            "value" => (float) $interval->value
+            "value" => (float) $interval->value,
+            "cost" => (float) $interval->cost / 100000,
           );
           $obj["end"] = $obj["start"] + $obj["duration"];
-          array_push($vars["intervals"], $obj);
+          $obj["costpersecond"] = $obj["cost"] / $obj["duration"];
+          $obj["costperminute"] = $obj["costpersecond"] * 60;
+          $obj["costperhour"] = $obj["costperminute"] * 60;
+          $obj["costperday"] = $obj["costperhour"] * 24;
+          array_push($intervals, $obj);
         }
       }
     }
-    return $this->GetComponentResponse("./smartmeter.tpl", $vars);
+    return $intervals;
+  }
+  public function getweather($date, $location) {
+    $apikey = "0e1afa84a2430ecc";
+    $url = sprintf("http://api.wunderground.com/api/%s/history_%d/q/%s.json", $apikey, $date, $location);
+
+    $cachedir = "components/smartmeter/data/weather/$location/";
+    $cachefile = $date . ".json";
+
+    if (file_exists($cachedir . $cachefile)) {
+      $json = file_get_contents($cachedir . $cachefile);
+    } else {
+      if (!(file_exists($cachedir) && is_dir($cachedir))) {
+        mkdir($cachedir, 0777, true);
+      }
+      $json = file_get_contents($url);
+      file_put_contents($cachedir . $cachefile, $json);
+    }
+    return json_decode($json);
   }
 }  
